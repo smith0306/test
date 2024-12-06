4222b2be784f2f1e7f17449934973b88cc48e386
naruhide21 <nkcclass0831@gmail.com>
2024-12-06 11:56:01 +0900

    No21クラスを作成

---
+++ Assets/Sandbox/No21.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns21
+{
+    public class No21 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
