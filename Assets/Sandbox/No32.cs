d4bde7173594f4df2f7a458dc203b199a8de7240
nkc-aito <nkc20242074@gmail.com>
2024-12-06 12:42:23 +0900

    Startメソッドの変更

--- Assets/Sandbox/No32.cs
+++ Assets/Sandbox/No32.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Ns32
@@ -9,7 +7,7 @@ namespace Ns32
         // Start is called before the first frame update
         void Start()
         {
-
+            Debug.Log("No32");
         }
 
         // Update is called once per frame
