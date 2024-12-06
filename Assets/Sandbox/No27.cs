1f33e6328528929b31ec55c84293b344a91cf97a
LanLaLi <kieulan9722@gmail.com>
2024-12-06 13:07:24 +0900

    Update No27

---
+++ Assets/Sandbox/No27.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Ns27
+{
+    public class No27 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("No27");
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
