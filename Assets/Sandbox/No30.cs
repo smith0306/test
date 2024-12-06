799c5297d6805d698ca6e287d335a5097e5711d8
nkc-keito <niwakeito0716@gmail.com>
2024-12-06 12:56:07 +0900

    No30クラスを作成

---
+++ Assets/Sandbox/No30.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns30
+{
+    public class No30 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("No30");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
