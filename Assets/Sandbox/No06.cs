36c8e6e9a269423d63908ce4081172c306f5bf80
smith0306 <harumakikmkm@gmail.com>
2024-12-10 11:31:15 +0900

    Create script

---
+++ Assets/Sandbox/No06.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns06{
+    public class No06 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
